342d83e0467f53fca430ca2dc16b4e8155421a6a
MatthewBart <MatthewCBartley@gmail.com>
2023-03-01 20:22:23 +0000

    Checkout and input loosely added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [items,setItems] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+       <input 
+        required
+        type="number"
+        value={items}
+        onChange={(e) => setItems(e.currentTarget.valueAsNumber)}
+       ></input>
+      <button>Checkout</button>  
+      </form>
     </div>
   );
 }
